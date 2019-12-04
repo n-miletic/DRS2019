8f0d329573c8acdd4e78f78aea9c7eb64788ab8f
BZug <zugicbogdan@gmail.com>
2019-12-04 17:18:49 +0100

    Added basic start menu

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QGridLayout, QPushButton, QGroupBox, QVBoxLayout, QLabel
 
 
 class Test(QWidget):
@@ -12,7 +12,36 @@ class Test(QWidget):
         self.setWindowTitle('Test')
         self.setGeometry(300, 150, 15 * self.size, 11 * self.size)
 
+        self.createGridLayout()
+
+        windowLayout = QVBoxLayout()
+        windowLayout.addWidget(self.horizontalGroupBox)
+        self.setLayout(windowLayout)
+
         self.show()
+
+    def createGridLayout(self):
+        self.horizontalGroupBox = QGroupBox("Grid")
+        layout = QGridLayout()
+        #layout.setColumnStretch(1, 4)
+        #layout.setColumnStretch(2, 4)
+
+        widget1 = QPushButton('Single Player')
+        widget1.setFixedSize(200,30)
+        widget2 = QPushButton('Multiplayer')
+        widget2.setFixedSize(200,30)
+        widget3 = QPushButton('Exit')
+        widget3.setFixedSize(200,30)
+
+        layout.addWidget(widget1, 1, 1)
+        layout.addWidget(widget2, 2, 1)
+        layout.addWidget(widget3, 3, 1)
+        layout.addWidget(QLabel(" "), 0, 1)
+        layout.addWidget(QLabel(" "), 4, 1)
+        layout.addWidget(QLabel(" "), 5, 1)
+        layout.addWidget(QLabel(" "), 6, 1)
+
+        self.horizontalGroupBox.setLayout(layout)
 
 
 if __name__ == '__main__':
